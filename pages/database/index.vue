32c956887c30020dee3a0b5a2f2acf60791a9752
Hannah <hanneysaurus@hotmail.com>
2024-10-10 18:23:57 +0200

    added search function in database (unstyled)

--- pages/database/index.vue
+++ pages/database/index.vue
@@ -1,8 +1,14 @@
 <template>
   <h1>Database</h1>
+  <div class="search">
+    <input
+        type="text"
+        @input="updateSearchQuery"
+    />
+  </div>
   <div class="database">
-    <div v-for="(d, index) in data" :key="index">
-      <MediaCard :media="d" :index="index" />
+    <div v-for="(d, index) in filteredData" :key="index">
+      <MediaCard :media="d" :index="index"/>
     </div>
   </div>
 </template>
@@ -16,6 +22,22 @@ const data = database;
 definePageMeta({
   layout: 'database'
 });
+
+const searchQuery = ref();
+const filteredData = computed(() => {
+  if (!searchQuery.value) {
+    return data;
+  }
+  return data.filter(media => {
+    const searchable = media.Title.toString().toLowerCase();
+    const query = searchQuery.value.toString().toLowerCase();
+    return searchable.includes(query);
+  });
+});
+
+const updateSearchQuery = (event) => {
+  searchQuery.value = event.target.value;
+};
 
 </script>
 
