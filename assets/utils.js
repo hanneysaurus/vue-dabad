ce6e98580a55dcdce1df6e329248f3884120cef7
Hannah <hanneysaurus@hotmail.com>
2026-02-21 01:02:46 +0100

    fixed naming and cleaning title

--- assets/utils.js
+++ assets/utils.js
@@ -3,6 +3,7 @@ export function getImageID(title) {
     title_cleaned = title_cleaned.replaceAll('ö', 'o');
     title_cleaned = title_cleaned.replaceAll('ä', 'a');
     title_cleaned = title_cleaned.replaceAll('ü', 'u');
+    title_cleaned = title_cleaned.replaceAll('é', 'e');
     title_cleaned = title_cleaned.replace(/[^a-zA-Z0-9 ]/g, '');
     title_cleaned = title_cleaned.replaceAll('__', '_');
     const image_id = title_cleaned.toLowerCase().replace(/\s+/g, '_');
